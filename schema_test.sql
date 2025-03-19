e183a4b4bb2a027cd48a0163697f00a75233be77
Timofeeva2303 <timofeeva023089@gmail.com>
2025-03-19 13:09:40 +0300

    Add files via upload

---
+++ schema_test.sql
@@ -0,0 +1,92 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.17 (Homebrew)
+-- Dumped by pg_dump version 17.0
+
+-- Started on 2025-03-19 13:09:11 MSK
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 210 (class 1259 OID 16391)
+-- Name: sales; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.sales (
+    sale_id character varying(50) NOT NULL,
+    branch_id character varying(50) NOT NULL,
+    user_id character varying(50) NOT NULL,
+    sku character varying(255),
+    quantity integer,
+    price integer,
+    total integer GENERATED ALWAYS AS ((quantity * price)) STORED
+);
+
+
+ALTER TABLE public.sales OWNER TO postgres;
+
+--
+-- TOC entry 209 (class 1259 OID 16385)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    user_id character varying(50) NOT NULL,
+    name character varying(255) NOT NULL,
+    "position" character varying(50) NOT NULL,
+    skill integer,
+    CONSTRAINT users_skill_check CHECK (((skill >= 1) AND (skill <= 10)))
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- TOC entry 3534 (class 2606 OID 16395)
+-- Name: sales sales_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sales
+    ADD CONSTRAINT sales_pkey PRIMARY KEY (sale_id);
+
+
+--
+-- TOC entry 3532 (class 2606 OID 16390)
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (user_id);
+
+
+--
+-- TOC entry 3535 (class 2606 OID 16396)
+-- Name: sales sales_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sales
+    ADD CONSTRAINT sales_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(user_id) ON DELETE CASCADE;
+
+
+-- Completed on 2025-03-19 13:09:11 MSK
+
+--
+-- PostgreSQL database dump complete
+--
+
